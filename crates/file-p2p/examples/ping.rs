b990919f871d58b89ce7fad3caa4571fc875b27f
BruceZhang54110 <zhw54110@163.com>
2025-05-17 00:23:03 +0800

    learn libp2p ping example

--- crates/file-p2p/examples/ping.rs
+++ crates/file-p2p/examples/ping.rs
@@ -1,8 +1,13 @@
 use anyhow::Result;
 
-use libp2p::{noise, ping, tcp, yamux};
+use libp2p::{noise, ping, tcp, yamux, Multiaddr};
 use tracing_subscriber::EnvFilter;
+use futures::prelude::*;
+use libp2p::swarm::SwarmEvent;
+use std::time::Duration;
 
+
+/// cargo run --package file-p2p --example ping
 #[tokio::main]
 async fn main() -> Result<()> {
     let _ = tracing_subscriber::fmt()
@@ -11,13 +16,37 @@ async fn main() -> Result<()> {
 
     // create new identity
     
-    let swarm = libp2p::SwarmBuilder::with_new_identity().with_tokio()
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity().with_tokio()
     // Transport
     .with_tcp(tcp::Config::default(), noise::Config::new, yamux::Config::default)?
         // Network behaviour
-        .with_behaviour(|_| ping::Behaviour::default())?.build();
+        .with_behaviour(|_| ping::Behaviour::default())?
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX)))
+        .build();
     
-    
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned
+    // port.
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    Ok(())
+    // Dial the peer identified by the multi-address given as the second
+    // command-line argument, if any.
+    if let Some(addr) = std::env::args().nth(1) {
+        let remote: Multiaddr = addr.parse()?;
+        swarm.dial(remote)?;
+        println!("Dialed {addr}")
+    }
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
+            SwarmEvent::ConnectionEstablished { peer_id, connection_id, endpoint, num_established, concurrent_dial_errors, established_in }
+                =>  {
+                    println!("connection established: {:?}, {:?}", peer_id, connection_id);
+                },
+                
+            SwarmEvent::Behaviour(event) => println!("{event:?}"),
+            _ => {}
+        }
+    }
+
 }
--- crates/quic-transport/src/quic_endpoint.rs
+++ crates/quic-transport/src/quic_endpoint.rs
@@ -1,11 +1,14 @@
 use anyhow::Ok;
 use quinn::{Endpoint, ClientConfig, ServerConfig};
+use rustls::pki_types::pem::PemObject;
 use rustls::pki_types::CertificateDer;
 use rustls::pki_types::PrivatePkcs8KeyDer;
 use std::net::SocketAddr;
 use anyhow::Result;
 use std::sync::Arc;
-
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::path::Path;
 
 #[derive(Debug)]
 pub struct ClientEndpoint {
@@ -15,8 +18,8 @@ pub struct ClientEndpoint {
 
 impl ClientEndpoint {
 
-    pub fn new(bind_addr: SocketAddr, server_certs: &[&[u8]]) -> Result<Self> {
-        let config = Self::generate_client_config(server_certs)?;
+    pub fn new(bind_addr: SocketAddr, server_cert_path: &Path) -> Result<Self> {
+        let config = Self::generate_client_config(server_cert_path)?;
         let mut endpoint = Endpoint::client(bind_addr)?;
         endpoint.set_default_client_config(config);
         Ok(ClientEndpoint {
@@ -26,51 +29,59 @@ impl ClientEndpoint {
     }
     
     /// Constructs a QUIC endpoint configured for use a client only.
-    fn generate_client_config(server_certs: &[&[u8]]) -> Result<ClientConfig> {
+    fn generate_client_config(server_cert_path: &Path) -> Result<ClientConfig> {
         let mut certs = rustls::RootCertStore::empty();
-        for cert in server_certs {
-            certs.add(CertificateDer::from(*cert))?;
-        }
+        certs.add(CertificateDer::from_pem_file(server_cert_path)?)?;
         ClientConfig::with_platform_verifier();
         Ok(ClientConfig::with_root_certificates(Arc::new(certs))?)
     }
+
+    pub fn get_endpoint(&self) -> &Endpoint {
+        &self.endpoint
+    }
+
+    
 
 
     
 }
 
 #[derive(Debug)]
-pub struct ServerEndpoint {
+pub struct ServerEndpointContainer {
     endpoint: Endpoint,
     bind_addr: SocketAddr,
-    certificate: CertificateDer<'static>,
 }
 
-impl ServerEndpoint {
+impl ServerEndpointContainer {
 
-    fn new(bind_addr: SocketAddr) -> Result<Self> {
-        let (server_config, cert) = Self::generate_server_config()?;
+    pub fn new(bind_addr: SocketAddr, cert_path: &Path, server_key_path: &Path) -> Result<Self> {
+        let server_config = Self::generate_server_config(cert_path, server_key_path)?;
         let endpoint = Endpoint::server(server_config, bind_addr)?;
-        Ok(ServerEndpoint {
+        Ok(ServerEndpointContainer {
             endpoint: endpoint,
             bind_addr: bind_addr,
-            certificate: cert,
         })
     }
     
     /// Constructs a QUIC endpoint configured for use a server only.
-    fn generate_server_config() -> Result<(ServerConfig, CertificateDer<'static>)> {
+    fn generate_server_config(cert_path: &Path, server_key_path: &Path) -> Result<ServerConfig> {
         // 使用 rcgen 生成自签名证书
-        let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
-        let cert_der = CertificateDer::from(cert.cert);
-        let priv_key = PrivatePkcs8KeyDer::from(cert.key_pair.serialize_der());
-
+        //let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
+        let cert_der = CertificateDer::from_pem_file(cert_path)?;
+        let priv_key = PrivatePkcs8KeyDer::from_pem_file(server_key_path)?;
         let mut server_config =
             ServerConfig::with_single_cert(vec![cert_der.clone()], priv_key.into())?;
         let transport_config = Arc::get_mut(&mut server_config.transport).unwrap();
         transport_config.max_concurrent_uni_streams(0_u8.into());
 
-        Ok((server_config, cert_der))
+        Ok(server_config)
+    }
+
+    pub fn get_endpoint(&self) -> &Endpoint {
+        &self.endpoint
+    }
+    pub fn get_bind_addr(&self) -> SocketAddr {
+        self.bind_addr
     }
     
 }
@@ -80,4 +91,18 @@ async fn run_client(endpoint: &Endpoint, server_addr: SocketAddr) {
     let connect = endpoint.connect(server_addr, "localhost").unwrap();
     let connection = connect.await.unwrap();
     println!("[client] connected: addr={}", connection.remote_address());
+}
+
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_client_endpoint() {
+        let addr1 = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 5000);
+        let server_endpoint = ServerEndpointContainer::new(addr1, Path::new("server.cert"), Path::new("server.key")).unwrap();
+        println!("[server] listening on: addr={:?}", server_endpoint.bind_addr);
+        let client_endpoint = ClientEndpoint::new(addr1, Path::new("server.cert")).unwrap();
+        println!("[client] listening on: addr={:?}", client_endpoint.endpoint.local_addr());
+
+    }
 }
